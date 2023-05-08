62c6308b0b64791e00f47b208b4ad67e9e80c1d9
Sakib Talukqder <126917076+sakibtalukqder@users.noreply.github.com>
2023-05-09 02:31:21 +0600

    Add files via upload

---
+++ auto_message.py
@@ -0,0 +1,16 @@
+# ________Requarements: pyautogui______
+
+import pyautogui
+import time
+
+#_____Time Deley_____//
+time.sleep(1)
+
+#______Message Times______//
+for i in range (100):
+    #_____The Messages______//
+    pyautogui.typewrite("Messages //")
+    pyautogui.press("enter")
+
+
+# _________Author: Sakib Talukqder___________//
